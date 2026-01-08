c9b322f5e85d225f337d4794391363fb8e2e15ad
1004aimbot-dev <1004aimbot@gmail.com>
2026-01-08 16:55:11 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-ReactDOM.createRoot(
-  document.getElementById('root')!
-).render(
+// ❗ index.css 없으면 이 줄 삭제
+// import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
