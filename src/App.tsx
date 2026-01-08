a1d64262ca850e55db17b2027874d44bfb6da03a
1004aimbot-dev <1004aimbot@gmail.com>
2026-01-08 16:04:52 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,15 @@
-import React from 'react';
-import Header from './components/Header';
-import Introduction from './components/Introduction';
-import MissionVision from './components/MissionVision';
-import Gallery from './components/Gallery';
-import News from './components/News';
-import Members from './components/Members';
-import Recruitment from './components/Recruitment';
-import Footer from './components/Footer';
-import AiCompanion from './components/AiCompanion';
-import { AdminProvider } from './contexts/AdminContext';
+import Header from './components/Header'
+import Introduction from './components/Introduction'
+import Footer from './components/Footer'
 
-const App: React.FC = () => {
+function App() {
   return (
-    <AdminProvider>
-      <div className="min-h-screen w-full relative">
-        <Header />
-        <Introduction />
-        <MissionVision />
-        <Gallery />
-        <News />
-        <Members />
-        <Recruitment />
-        <Footer />
-        <AiCompanion />
-      </div>
-    </AdminProvider>
-  );
-};
+    <>
+      <Header />
+      <Introduction />
+      <Footer />
+    </>
+  )
+}
 
-export default App;
+export default App
